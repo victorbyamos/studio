ac5f31cb54da004969a438fbe73b5af5edfdf382
Money Online <idealmoneyinfo@gmail.com>
2025-08-27 09:09:23 +0000

    Error: Hydration failed because the server rendered HTML didn't match th

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,11 +14,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
-      </head>
+      <head />
       <body className="font-body antialiased">
         <div className="flex flex-col min-h-screen">
           <main className="flex-1">{children}</main>
